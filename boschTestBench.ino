6176e247f476f7aff59ae992cfdd9d1988cfb7a5
aabdelghani <ahmedabdelghany15@gmail.com>
2024-01-02 00:26:42 +0200

    added the Doxygen Documentation Feature

--- boschTestBench.ino
+++ boschTestBench.ino
@@ -1,47 +1,78 @@
+/**
+ * @file pwm_timer_interrupt.ino
+ * @brief PWM Generation using Timer1 with Selectable Frequencies on Arduino
+ *
+ * This program is designed to generate a PWM (Pulse Width Modulation) signal
+ * using Timer1 interrupts on an Arduino. It allows the user to select
+ * predefined frequencies for the PWM signal.
+ *
+ * @author Ahmed Abdelghany
+ * @date 1/2/2024
+ */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-const int ledPin = 9; // Use a pin compatible with Timer1
+const int ledPin = 9; ///< PWM output pin, connected to LED.
 
-// Define your desired frequency here
-#define FREQUENCY 1kHz
+/**
+ * Predefined frequencies for PWM signal.
+ * FREQUENCY can be set to any of these.
+ */
+#define FREQ_1KHZ 1999  ///< Frequency 1kHz (Timer1 compare match value).
+#define FREQ_2KHZ 999   ///< Frequency 2kHz (Timer1 compare match value).
+#define FREQ_5KHZ 399   ///< Frequency 5kHz (Timer1 compare match value).
 
-// Predefined frequencies
-#define FREQ_1KHZ 1999  // For 1kHz
-#define FREQ_2KHZ 999   // For 2kHz
-#define FREQ_5KHZ 399   // For 5kHz
-// Add more frequencies as needed
+#define FREQUENCY 1kHz  ///< Set the desired frequency here.
 
+/**
+ * @brief Sets up Timer1 in CTC (Clear Timer on Compare Match) mode.
+ *
+ * This function initializes Timer1 and sets the compare match register (OCR1A)
+ * based on the selected frequency. It also configures the timer interrupt
+ * to trigger on compare match.
+ */
 void setup() {
   pinMode(ledPin, OUTPUT);
 
-  // Set up Timer1
-  noInterrupts();           // Disable all interrupts
-  TCCR1A = 0;               // Set entire TCCR1A register to 0
-  TCCR1B = 0;               // Same for TCCR1B
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
 
-  // Set compare match register based on the chosen frequency
+  // Select compare match register value based on predefined frequencies.
   #if FREQUENCY == 1kHz
     OCR1A = FREQ_1KHZ;
   #elif FREQUENCY == 2kHz
     OCR1A = FREQ_2KHZ;
   #elif FREQUENCY == 5kHz
     OCR1A = FREQ_5KHZ;
-  // Add more cases as needed
   #else
     #error "Frequency not defined"
   #endif
 
-  TCCR1B |= (1 << WGM12);   // Turn on CTC mode
-  TCCR1B |= (1 << CS11);    // Set CS11 bit for 8 prescaler
-  TIMSK1 |= (1 << OCIE1A);  // Enable timer compare interrupt
-  interrupts();             // Enable all interrupts
+  TCCR1B |= (1 << WGM12);
+  TCCR1B |= (1 << CS11);
+  TIMSK1 |= (1 << OCIE1A);
+  interrupts();
 }
 
+/**
+ * @brief Interrupt Service Routine for Timer1 Compare Match.
+ *
+ * The ISR toggles the LED pin at each timer compare match,
+ * creating the PWM signal.
+ */
 ISR(TIMER1_COMPA_vect) {
-  // ISR code remains the same
+  static bool ledState = false;
+  digitalWrite(ledPin, ledState);
+  ledState = !ledState;
 }
 
+/**
+ * @brief Main loop function.
+ *
+ * This function runs repeatedly after setup function.
+ */
 void loop() {
-  // Nothing needed here
+  // Empty loop since PWM is handled by Timer interrupt.
 }
