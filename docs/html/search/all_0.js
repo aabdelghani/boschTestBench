f97a3cf7fe16d197680975fe8d2ac9af352972e1
aabdelghani <ahmedabdelghany15@gmail.com>
2024-01-02 21:26:23 +0200

    Fixed Doxygen issue of not showing on the github Actions

--- docs/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['arduino_20pwm_20generation_20using_20timer1_20with_20selectable_20frequencies_0',['Arduino PWM Generation Using Timer1 with Selectable Frequencies',['../md__c___test_bench_bosch_test_bench__r_e_a_d_m_e.html',1,'']]]
-];
